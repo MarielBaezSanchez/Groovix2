0944cf1019fe9fda04f96dd2f014dbedf1d959d8
jesus <jesusrguez970@gmail.com>
2025-12-07 13:19:27 -0600

    probanding

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from "virtual:pwa-register";
 registerSW()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Groovix2",
+        short_name: "Groovix",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 
   server: {
     host: true,
     port: 5173,
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://server-e7g2.onrender.com",
     },
   }
 });
